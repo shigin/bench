72c8fb31e3a38ba46197db22389fb15a7f45dc76
Alexander Shigin <shigin@rambler-co.ru>
2009-12-28 19:34:35 +0300

    remove empty changelog and news

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,4 @@ autoheader || exit 1
 mkdir -p aclocal || exit 1
 aclocal -I ./aclocal || exit 1
 autoconf || exit 1
-automake -ac --add-missing || exit 1
+automake -ac --add-missing --foreign || exit 1
